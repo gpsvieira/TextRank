e8469f3cf8d68255be7ec2bf3324b49b893e2253
Thorben Werner <w.thorben94@gmail.com>
2018-06-29 11:51:21 +0200

    sliding window for keyword extraction

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from plotting_example import plot_a_graph
+from plotting_example import histogram
 import operator
 
 #from template import textRank
@@ -8,5 +9,6 @@ G, pr = textRank(file="corpora/hp.txt")
 
 sorted_pr = sorted(pr.items(), key=operator.itemgetter(1))
 sorted_pr = sorted_pr[::-1]
-print(sorted_pr[0:10])
-plot_a_graph(G)
+print(sorted_pr[0:100])
+histogram(sorted_pr)
+# plot_a_graph(G)
